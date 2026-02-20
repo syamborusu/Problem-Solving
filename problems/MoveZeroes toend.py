79a105fa621322128aab33c1972ed3ea8d63e27f
syam <borusulokesh0350@gmail.com>
2026-02-20 09:04:28 +0530

    Feb 20th practices

--- problems/MoveZeroes toend.py
+++ problems/MoveZeroes toend.py
@@ -1,14 +1,24 @@
 arr = [0, 1, 0, 3, 12]
 pos = 0                   #posistion is intialize to 0th index
 for i in range(len(arr)):        #loop starts from 0 to last index of arr
-    if arr[i] != 0:              #0th index i not equalaa to 0
-        arr[pos] = arr[i]        # pos is assigend with ith value
+    if arr[i] != 0:              #0th index i not equal to 0
+        arr[pos] = arr[i]        # pos is assigend with i th value
         pos += 1                 #Pos increment by 1 posistion ex:pos=1th index
 while pos < len(arr):            #loop runs from value of pos to len of arr
     arr[pos] = 0                 #pos value is reassigend with zero
     pos += 1                     #pos will increment by 1 untill last element
 print(arr)
 #TC=O(N),SC=O(1)
+
+#better optimal sol
+nums= [0, 1, 0, 3, 12]
+l = 0
+for r in range(len(nums)):
+    if nums[r] != 0:
+        nums[l], nums[r] = nums[r], nums[l]
+        l += 1
+print(nums)
+
 
 #Brute force
 
